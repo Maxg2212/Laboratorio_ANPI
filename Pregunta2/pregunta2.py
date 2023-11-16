37001b61618deff11c14764ac575e981cd29f260
Max Garro Mora <maxgarro22@gmail.com>
2023-11-16 01:11:42 -0600

    [Update] Pregunta 2 corregida

--- Pregunta2/pregunta2.py
+++ Pregunta2/pregunta2.py
@@ -1,13 +1,18 @@
 import numpy as np
 import time
-import SVDCompact
 import matplotlib.pyplot as mplt
+
+from Pregunta1 import SVDCompact
+
 V = np.array([5,6,7,8,9,10,11,12,13])
 
 n = len(V)
 
 tiemposSVD = np.zeros(n)
 tiemposSVDCompact = np.zeros(n)
+
+tiempo1 = []
+tiempo2 = []
 
 for i in range(0,n):
     K = V[i]
@@ -17,16 +22,22 @@ for i in range(0,n):
     U1,S1,V1 = np.linalg.svd(A) #Se usa operacion prefabricada
     et1 = time.time()-t1
     tiemposSVD[i] = et1
+    #tiempo1.append(et1)
     print(et1)
 
     t2 = time.time()
     U2,S2,V2 = SVDCompact.svdCompact(A)
     et2 = time.time()-t2
     tiemposSVDCompact[i] = et2
+    #tiempo2.append(et2)
     print(et2)
+#V = np.array(V)
+#mplt.figure()
 
 mplt.plot(V, tiemposSVD)
 mplt.plot(V, tiemposSVDCompact)
+#mplt.plot(V, tiempo1, 'r')
+#mplt.plot(V, tiempo2, 'b')
 mplt.legend()
 mplt.show()
 
