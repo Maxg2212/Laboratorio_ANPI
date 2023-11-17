1fe228227795e608b93c715dc012c21f8092ae58
Max Garro Mora <maxgarro22@gmail.com>
2023-11-16 19:15:55 -0600

    [Update] Correcciones

--- Pregunta2/SVDCompact.py
+++ Pregunta2/SVDCompact.py
@@ -3,7 +3,6 @@ import numpy as np
 
 def svdCompact(A):
     m, n = np.shape(A)
-    #m, n = len(A), len(A[0])
 
     if m>n:
         M1 = np.dot(np.transpose(A), A)
@@ -16,7 +15,6 @@ def svdCompact(A):
         y3 = y1 * y2
         s1 = np.sort((np.sqrt(y3)))[::-1]
         order = np.array(list(range(len(s1))))[::-1]
-        #order = np.argsort(s1)[::-1]
 
         V2 = V1[:, order]
 
@@ -25,7 +23,7 @@ def svdCompact(A):
         Sr = s1[0:rA]
         
         Ur = (1/np.transpose(Sr))*(np.dot(A, Vr))
-        #return Ur, Sr, Vr
+
     else:
         M1 = np.dot(A, np.transpose(A))
 
@@ -40,7 +38,6 @@ def svdCompact(A):
 
         order = np.array(list(range(len(s1))))[::-1]
 
-        #order = np.argsort(s1)[::-1]
 
         U2 = U1[:,order]
 
@@ -50,60 +47,7 @@ def svdCompact(A):
 
         Vr = (1/np.transpose(Sr))*np.dot(np.transpose(A), Ur)
     return Ur, Sr, Vr
-"""
 
-
-def svdCompact(A):
-
-    m, n = len(A), len(A[0])
-
-    if m > n:
-        M1 = np.dot(np.transpose(A), A)
-        D, V1 = np.linalg.eig(M1)
-        y1 = D
-        const = n * np.max(y1) * np.finfo(np.float64).eps
-        y2 = y1 > const
-        rA = np.sum(y2)
-
-        y3 = y1 * y2
-        s1 = np.sort((np.sqrt(y3)))[::-1]
-        order = np.array(list(range(len(s1))))[::-1]
-        V2 = V1[:, order]
-
-        Vr = V2[:, 0:rA]
-
-        Sr = s1[0:rA]
-
-        Ur = (1 / np.transpose(Sr)) * (np.dot(A, Vr))
-        return Ur, Sr, Vr
-    else:
-        M1 = np.dot(A, np.transpose(A))  # producto punto
-
-        D, U1 = np.linalg.eig(M1)
-
-        y1 = D
-
-        const = m * np.max(y1) * np.finfo(np.float64).eps
-
-        y2 = (y1 > const).astype(int)
-
-        rA = np.sum(y2)
-
-        y3 = y1 * y2  # multiplicacion punto a punto
-
-        s1 = np.sort((np.sqrt(y3)))[::-1]
-
-        order = np.array(list(range(len(s1))))[::-1]
-
-        U2 = U1[:, order]
-
-        Ur = U2[:, 0:rA]
-
-        Sr = s1[0:rA]
-        Vr = (1 / np.transpose(Sr)) * np.dot(np.transpose(A), Ur)
-        return Ur, Sr, Vr
-
-"""
 
 
 
--- Pregunta3/pregunta3.py
+++ Pregunta3/pregunta3.py
@@ -20,11 +20,10 @@ for k in range(1, sizeLength + 1):
         directory = os.path.join('training', f's{k}', f'{m}.jpg')
         A = cv2.imread(directory, cv2.IMREAD_GRAYSCALE)
         B = np.divide(A.astype('float'), 255.0)
-        #print(directory)
         x = B.flatten()
         S[:, columna_s] = x
         columna_s += 1
-    #print(S)
+
 
 
 # Paso 2
@@ -35,19 +34,16 @@ for i in range(totalPixels):
 
 
 fProm = 1 / totalImg * fProm
-#print(fProm)
 
 # Paso 3 y 4
 A = np.zeros((totalPixels, totalImg))
 for i in range(totalImg):
     A[:,i] = S[:,i] - fProm.flatten()
-#print(A)
 
 
 
 # Paso 5
 U, S, V = SVDCompact.svdCompact(A)
-#print(U)
 
 
 # Paso 13
@@ -57,25 +53,17 @@ for n in range(1, sizeLength + 1):
     direccion = os.path.join('compare', f'p{n}.jpg')
     A_compare = cv2.imread(direccion, cv2.IMREAD_GRAYSCALE)
     B_compare = np.divide(A_compare.astype('float'), 255.0)
-    #print(direccion)
     f = B_compare.flatten()
-    #print(f)
     x = np.dot(U.conj().T,  (f - fProm.flatten()))
-    #print(x)
     
     for k in range(1, sizeLength + 1):
         for m in range(1, numImages + 1):
             trainingDirectory = os.path.join('training', f's{k}', f'{m}.jpg')
             A_training = cv2.imread(trainingDirectory, cv2.IMREAD_GRAYSCALE)
-            #print(trainingDirectory)
-            #direccion = f'training/s{k}/{m}.jpg'
-            #A_training = cv2.imread(direccion)
             B_training = np.divide(A_training.astype('float'), 255.0)
-            #print("Entro al for 2 del paso 13")
             f_i = B_training.flatten()
             x_i = np.dot(U.conj().T,  (f_i - fProm.flatten()))
             dif = np.linalg.norm(x - x_i)
-            #dif = (np.dot(np.transpose((x-xi)), (x-xi)))**(1/2)
 
             if minValue == 0:
                 minValue = dif
